4492531f5ad027aaad997da3a04b110ec2b7f035
ChiaraTilaro <chiara.tilaro@gmail.com>
2026-06-01 09:24:43 +0200

    lab 8 completo

--- model/model.py
+++ model/model.py
@@ -7,21 +7,68 @@ class Model:
         self._listNerc = None
         self._listEvents = None
         self.loadNerc()
+        self._clientiMaxBest = 0
 
 
 
     def worstCase(self, nerc, maxY, maxH):
         # TO FILL
-        pass
+        self.loadEvents(nerc)
+        parziale = []
+        self.ricorsione(parziale , maxY , maxH, 0)
     def ricorsione(self, parziale, maxY, maxH, pos):
         # TO FILL
-        pass
+        # terminazione
+        if self.sumDurata(parziale)/60/60 > maxH :
+            return
+
+        # verifica se best
+        if self.countCustomers(parziale) > self._clientiMaxBest:
+            self._solBest = parziale[:]
+            self._clientiMaxBest = self.countCustomers(parziale)
+
+        #ricorsione
+        i = pos
+        for e in self._listEvents[pos:]:
+            parziale.append(e)
+            if self.getRangeAnni(parziale) > maxY:
+                parziale.remove(e)
+                return
+            i +=1
+            self.ricorsione(parziale , maxY , maxH , i)
+            parziale.remove(e)
+
+    def getRangeAnni(self , listOutages):
+        if len(listOutages) <2:
+            return 0
+        first = listOutages[0].date_event_began
+        last = listOutages[-1].date_event_finished
+        return int(last.year - first.year)
+
+    def countCustomers(self , listOutages):
+        if len(listOutages) ==0:
+            return 0
+        numCustomers = 0
+        for event in listOutages:
+            numCustomers += event.customers_affected
+        return numCustomers
+
+    def sumDurata(self , listOutages):
+        if len(listOutages) == 0:
+            return 0
+        sum =0
+        for event in listOutages:
+            sum += self.durata(event)
+        return sum
 
     def loadEvents(self, nerc):
         self._listEvents = DAO.getAllEvents(nerc)
 
     def loadNerc(self):
         self._listNerc = DAO.getAllNerc()
+
+    def durata(self, event):
+        return (event.date_event_finished-event.date_event_began).total_seconds()
 
 
     @property
